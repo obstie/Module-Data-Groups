9de65114ef114fddd427143796083444d156ab23
obie <obertilimandzi@gmail.com>
2025-07-09 16:51:51 +0200

    tested my  dedupe functions seems to work

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -1,3 +1,6 @@
+
+
+
 // Fix this implementation
 // Start by running the tests for this function
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
@@ -5,10 +8,63 @@
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
+
+
+
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+     if (!Array.isArray(list)) return null;
+          const numFilter = list.filter(item => typeof item === "number"); // Filter out non-numeric values 
+
+     if (numFilter.length === 0) return null; // Return null if there are no numbers
+
+          const sorted = [...numFilter].sort((a, b) => a - b) ; // Sort the numbers in ascending order make a copy and sort them
+          const midNum = Math.floor(sorted.length / 2);
+
+     if (sorted.length % 2 === 0) {
+          return (sorted[midNum - 1] + sorted[midNum]) / 2; // Returns average of two middle numbers for even arrays
+     } 
+     else {
+          return sorted[midNum];// Returns middle number for odd arrays
+     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = calculateMedian;
--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,13 @@
-function dedupe() {}
+function dedupe(array) {
+  return [...new Set(array)];   
+}
+
+const filteredNum = dedupe(['a', 'a', 'a', 'b', 'b', 'c', 'd', 'd', 'e', 'e', 'f', 'f ']);
+const emptyArray = dedupe([]);
+
+
+console.log(emptyArray)
+console.log(filteredNum);
+
+
+
