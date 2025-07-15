4e9a6d2712c4758072d1a54322aad6eba2fc9f05
obie <obertilimandzi@gmail.com>
2025-07-15 23:31:20 +0200

    code is working i can loop through the object

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -3,6 +3,8 @@
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
+// it wont be able to loop through the object using a for of loop because it is not iterable 
+
 const author = {
   firstName: "Zadie",
   lastName: "Smith",
@@ -11,6 +13,10 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value in author) {
+  //
   console.log(value);
+  console.log(author[value]); // this will print the value 
 }
+
+// i see you cannot iterarte over an object using a for of loop, you use for in loop
