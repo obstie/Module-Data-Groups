fd0b80c8bad837b5bb641b5becd68198bd9bfbb8
obie <obertilimandzi@gmail.com>
2025-07-16 00:01:13 +0200

    fixed the code and can loop through the ingredients

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -3,13 +3,17 @@
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
+// we will use a for of loop to go through the ingredients array
 
 const recipe = {
   title: "bruschetta",
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
-
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+  
+console.log(`${recipe.title} serves ${recipe.serves}`);
+console.log("ingredients:");
+// you log these above first then then you loop through the ingredients and then print
+for (const ingredient of recipe.ingredients) {
+  console.log(ingredient);
+} 
