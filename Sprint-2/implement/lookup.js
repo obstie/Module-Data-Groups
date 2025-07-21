dd20e65f56cc4ab4b2ca6d79416f47a2b37397a0
obie <obertilimandzi@gmail.com>
2025-07-21 22:10:59 +0200

    tested my code and seems to be fine

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,18 @@
-function createLookup() {
-  // implementation here
+
+function createLookup(inputArray) {
+       const result = {};
+  for (const item of inputArray) {
+    if (Array.isArray(item) && item.length === 2) { // Check if item is an array with two elements to avoid errors
+     
+      const countyCode = item[0]; // key
+      const currencyOfCounty = item[1]; // value
+      result[countyCode] = currencyOfCounty; 
+    }
+    else console.error("Input must be an array of arrays");
+  }
+   return result
 }
+
+console.log(createLookup([['US', 'USD'], ['CA', 'CAD']]));
 
 module.exports = createLookup;
